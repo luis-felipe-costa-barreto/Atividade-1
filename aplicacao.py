f8bed86a9c0eac8757183c0f627d230eb645dfc4
felipetorresf1 <felipenews2016@gmail.com>
2024-07-18 08:08:36 -0300

    aplicacoes

---
+++ aplicacao.py
@@ -0,0 +1,7 @@
+from exemplo import *
+
+x = Funcionario('costa', 123, 2000)
+y = Operario('felipe', 456, 1500, 'Manuseio dos fornos')
+print(x.get_cpf())
+x.set_salario(3000)
+print(y.get_departamento())
